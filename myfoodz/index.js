d767470553e7299973d7a1bff9d41f65e76850f8
ChrisSchmitz92 <christopher.schmitz@smail.th-koeln.de>
2020-12-10 17:42:56 +0100

    code formatiert

--- myfoodz/index.js
+++ myfoodz/index.js
@@ -8,31 +8,31 @@ const PORT = 3000
 
 app.get("/restaurants", async (req, res) => {
 
-  try {   
+  try {
 
-      const {data} = await 
-      
+    const { data } = await
+
       axios.get(
         `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=51.0266668,7.569283&radius=500&type=restaurant&key=${process.env.API_KEY}`
-      ).then(function (response){
+      ).then(function (response) {
         console.log(response.data)
-      
+
       })
 
-      status()
-      
+    status()
+
     res.json(data)
 
   }
-  catch(err) {
-    
+  catch (err) {
+
   }
 
 })
 
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`Server running on PORT: ${PORT}`)
 });
 
