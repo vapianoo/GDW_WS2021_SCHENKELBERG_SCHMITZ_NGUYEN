bcf97f5bb44e4fb9e184c8a060d47a4806565a20
ChrisSchmitz92 <christopher.schmitz@smail.th-koeln.de>
2020-12-10 16:07:46 +0100

    Radius angepasst

--- myfoodz/index.js
+++ myfoodz/index.js
@@ -13,7 +13,7 @@ app.get("/restaurants", async (req, res) => {
       const {data} = await 
       
       axios.get(
-        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=51.0266668,7.569283&radius=1000&type=restaurant&key=${process.env.API_KEY}`
+        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=51.0266668,7.569283&radius=500&type=restaurant&key=${process.env.API_KEY}`
       ).then(function (response){
         console.log(response.data)
       
